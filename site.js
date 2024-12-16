230b6540ee7e9584a8387e669cb3a06c6fa16dd1
viacheslavk55 <525vkostenko@frhsd.com>
2024-12-16 10:35:52 -0500

    Added the title and github owner using vue, step 3

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+            title: "IMDB + Slav's Top 8 Movies",
+            owner: "Slav",
+            github: "https://github.com/viacheslavk55/Movie-Gallery"
       }
     },
       methods: {
